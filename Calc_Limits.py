74db2ab3ab835cd037ed997c381bb6f29dbce2e2
AaronvanderGraaf <aaron.vandergraaf@tu-dortmund.de>
2023-06-07 13:30:46 +0200

    Updated Limit_calc script

--- Calc_Limits.py
+++ Calc_Limits.py
@@ -11,21 +11,21 @@ def Plot_Fit(fit_result, wc, xs, string):
     plt.plot(wc, fit_function(wc, *fit_result), c='black', label=string + " Parameter = " + str(np.round(fit_result[0], 2)))
     plt.xlabel('WC')
     plt.ylabel('cross section (pb)')
-    plt.title('cuu')
     plt.legend()
-    plt.show()
+    plt.savefig(string+".png")
+    #plt.show()
 
 def Format_Results(limit_array, string):
-    result = np.round(np.array([limit_array[0], limit_array[0]-limit_array[1], limit_array[0]-limit_array[2]]), precision)
+    result = np.round(np.array([limit_array[0], abs(limit_array[0]-limit_array[1]), abs(limit_array[0]-limit_array[2])]), precision)
 
     #print(string+"="+str(result[0])+"_{"+str(result[1])+"}^{+"+str(result[2])+"}")
     #print(f'={result[0]:.3f}_{result[1]:.3f}^{+{result[1]:.3f}}')
-    print(string+ f' = {result[0]:.3f}_({result[1]:.3f})_(+{result[2]:.3f})')
+    print(string+ f' = {result[0]:.3f}_(-{result[1]:.3f})_(+{result[2]:.3f})')
 
     return result
 
 # Settings
-plotting=False
+plotting=True
 precision = 3
 nom_signal_norm = 1.94 + 0.048
 Final_Norm = 0.4 * 0.05
@@ -54,17 +54,9 @@ uu_xs_norm = uu_xs_norm_tt + uu_xs_norm_tbartbar
 qu1_xs_norm = qu1_xs_norm_tt + qu1_xs_norm_tbartbar
 qu8_xs_norm = qu8_xs_norm_tt + qu8_xs_norm_tbartbar
 
-print(uu_xs_norm)
-print(qu1_xs_norm)
-print(qu8_xs_norm)
-
 uu_xs_norm_final = uu_xs_norm * Final_Norm
 qu1_xs_norm_final = qu1_xs_norm * Final_Norm
 qu8_xs_norm_final = qu8_xs_norm * Final_Norm
-
-print(uu_xs_norm_final)
-print(qu1_xs_norm_final)
-print(qu8_xs_norm_final)
 
 # Noemi Values
 #uu_xs_norm = np.array([3.17, 7.13, 12.68, 19.81])
@@ -75,30 +67,40 @@ wc_uu = np.array([0.02, 0.03, 0.04, 0.05])
 wc_qu1 = np.array([0.05, 0.07, 0.1, 0.15])
 wc_qu8 = np.array([0.1, 0.2, 0.25, 0.3])
 
-
 nom_xs = nom_signal_norm * nom_mu
+
 uu_wc_limit = []
 qu1_wc_limit = []
 qu8_wc_limit = []
 
 for i in range(uu_mu.size):
 
-    uu_xs = uu_xs_norm * uu_mu[i]
-    qu1_xs = qu1_xs_norm * qu1_mu[i]
-    qu8_xs = qu8_xs_norm * qu8_mu[i]
+    uu_xs = uu_xs_norm
+    qu1_xs = qu1_xs_norm
+    qu8_xs = qu8_xs_norm
+    #uu_xs = uu_xs_norm * uu_mu[i]
+    #qu1_xs = qu1_xs_norm * qu1_mu[i]
+    #qu8_xs = qu8_xs_norm * qu8_mu[i]
 
     fit_uu, _ = curve_fit(fit_function, wc_uu, uu_xs, p0=1.0)
     fit_qu1, _ = curve_fit(fit_function, wc_qu1, qu1_xs, p0=1.0)
     fit_qu8, _ = curve_fit(fit_function, wc_qu8, qu8_xs, p0=1.0)
 
     if plotting == True:
-        Plot_Fit(fit_uu, wc_uu, uu_xs, "Fit uu")
-        Plot_Fit(fit_qu1, wc_qu1, qu1_xs, "Fit qu1")
-        Plot_Fit(fit_qu8, wc_qu8, qu8_xs, "Fit qu8")
+        Plot_Fit(fit_uu, wc_uu, uu_xs, "Fit_uu")
+        Plot_Fit(fit_qu1, wc_qu1, qu1_xs, "Fit_qu1")
+        Plot_Fit(fit_qu8, wc_qu8, qu8_xs, "Fit_qu8")
 
-    uu_wc_limit.append(np.round(np.sqrt(nom_xs/fit_uu[0]), precision))
-    qu1_wc_limit.append(np.round(np.sqrt(nom_xs/fit_qu1[0]), precision))
-    qu8_wc_limit.append(np.round(np.sqrt(nom_xs/fit_qu8[0]), precision))
+    uu_wc_limit.append(np.round(np.sqrt(uu_xs_norm_final[0]*uu_mu[i]/fit_uu[0]), precision))
+    qu1_wc_limit.append(np.round(np.sqrt(qu1_xs_norm_final[0]*qu1_mu[i]/fit_qu1[0]), precision))
+    qu8_wc_limit.append(np.round(np.sqrt(qu8_xs_norm_final[0]*qu8_mu[i]/fit_qu8[0]), precision))
+
+#print(uu_wc_limit)
+#print(qu1_wc_limit)
+#print(qu8_wc_limit)
+
+print(uu_xs_norm_final)
+print()
 
 Format_Results(uu_wc_limit, "Cuu: ")
 Format_Results(qu1_wc_limit, "Cqu1:")
